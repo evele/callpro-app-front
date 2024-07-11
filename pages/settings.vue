86f5196acb0f1474a63c89ce7959c8403b06b8b8
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-07-11 11:07:46 -0300

    first commit

---
+++ pages/settings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p class="text-title">Settings page</p>
+    </div>
+</template>
+
+<script setup>
+    import { useSettingsStore } from "@/stores"
+
+    const settingsStore = useSettingsStore()
+
+    onMounted(async() => {
+        await settingsStore.loadSettingsData()
+    })
+</script>
+
+<style scoped>
+    .text-title {
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+    }
+</style>
