87bc0ea0388c31b18669edd5d60d4fb831ac3df8
Eric <inux2012@gmail.com>
2024-07-11 21:00:48 -0300

    now yes, useFetch fixed

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,4 +1,15 @@
 <template>
 
     <p>Logged IN :D</p>
+    <button type="button" @click=logout>Logout</button>
 </template>
+<script setup>
+
+import { useAuthStore } from "@/stores"
+
+const authStore = useAuthStore()
+function logout() {
+
+  authStore.logout()
+}
+</script>
