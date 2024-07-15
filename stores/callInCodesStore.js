aaa410970396c20ba3702d80e8efafc94a201e9d
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-07-15 12:15:31 -0300

    added create and delete functions

--- stores/callInCodesStore.js
+++ stores/callInCodesStore.js
@@ -13,5 +13,17 @@ export const useCallInCodesStore = defineStore("CallInCodesStore", {
         this.user_call_in_codes = response.user_call_in_codes
       }
     },
-  },
+    async createUserCallInCode(data) {
+      const response = await fetchWrapper.post(CREATE_CALL_IN_CODE_URL, data)    
+      if(response.result) {
+        this.loadUserCallInCodes()
+      }
+    },
+    async deleteUserCallInCode(id) {
+      const response = await fetchWrapper.post(DELETE_CALL_IN_CODE_URL, id)    
+      if(response.result) {
+        this.loadUserCallInCodes()
+      }
+    },
+  }
 })
--- utils/urls.js
+++ utils/urls.js
@@ -11,3 +11,5 @@ export const LOAD_SETTINGS_DATA_URL = "/settings/get_settings_data"
 
 /* ----- Call In Codes Store ----- */
 export const LOAD_USER_CALL_IN_CODES_URL = "/call_in_codes/get_user_call_in_codes"
+export const CREATE_CALL_IN_CODE_URL = "/call_in_codes/create_user_call_in_code"
+export const DELETE_CALL_IN_CODE_URL = "/call_in_codes/delete_user_call_in_code"
