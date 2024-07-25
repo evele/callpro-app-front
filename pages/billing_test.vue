1de61c41d8c49662b505c58d93463a71bffaf490
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-07-25 08:06:46 -0300

    testing with original script

---
+++ pages/billing_test.vue
@@ -0,0 +1,163 @@
+<template>
+    <div :key="renderKey">
+        <p class="text-title">Billing test page</p>
+        <div>
+            <button type="button" @click="show_add_card_form">Add New Card</button>
+            <p>{{ script_loaded_message }}</p>
+            <p v-if="showSuccess" style="color: green;">Success!</p>
+            <p v-if="showError" style="color: red;">Error!</p>
+            <br />
+            <br />
+            <br />
+
+            <div class="js-card-container hidden-xs push-50"></div>
+            <form class='js-card-form' data-encryptedfields="encrypted-form">
+                <div class="form-group">
+                    <div class="col-xs-12">
+                        <div class="form-material">
+                            <label for="number">Number</label>
+                            <input class="form-control" id="number" type="text" placeholder="**** **** **** ****">
+                            <input class="form-control hidden" id="enc-number" type="text" data-encryptedfields="cc" placeholder="**** **** **** ****">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-xs-12">
+                        <div class="form-material">
+                            <label for="cc-name">Full Name</label>
+                            <input class="form-control" id="cc-name" name="checkout-cc-name" type="text" placeholder="JOHN DOE">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-xs-6">
+                        <div class="form-material">
+                            <label for="expiry">MMYY</label>
+                            <input class="form-control" id="expiry" name="checkout-cc-expiry" type="text" placeholder="0119">
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <div class="form-material">
+                            <label for="cvv">CVC</label>
+                            <input class="form-control" id="cvv" data-encryptedfields="cvv" type="text" placeholder="***">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="block-content block-content-full bg-gray-lighter text-center">
+                        <button id='add-card-button' type="button">Add Card</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { useCreditCardsStore } from "@/stores"
+
+    const creditCardsStore = useCreditCardsStore()
+
+    const isLoading = ref(true)
+    const isAddingCard = ref(false)
+    const isSavingCard = ref(false)
+    const showSuccess = ref(false)
+    const showError = ref(false)
+    const renderKey = ref(0)
+    const script_loaded_message = ref('No cargó aún')
+
+    const cardNumber = ref('')
+    const cardName = ref('')
+    const cardMMYY = ref('')
+
+
+    const loadScript = () => {
+        const script = document.createElement('script');
+        script.src = "https://cdn.cardknox.com/js/v1/encryptedfields.js";
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            renderKey.value += 1
+            script_loaded_message.value = 'Script cargado'
+        }
+    }
+
+    onMounted(() => {
+        console.log('ready')
+        loadScript()
+    })
+
+    const formatExpiry = (event) => {
+      let input = event.target.value.replace(/\D/g, '');
+
+      if (input.length >= 3) {
+        input = input.substring(0, 2) + ' / ' + input.substring(2, 4);
+      }
+
+      event.target.value = input;
+      cardMMYY.value = input;
+    };
+
+    const show_add_card_form = () => {
+        isAddingCard.value = true
+    }
+
+    const confirm_add_card = () => {
+        const dataToSend = {
+            cc_number: '',
+            cc_name: cardName.value,
+            expiry: cardMMYY.value,
+            enc_card_number,
+            cvv: '286'
+        }
+        console.log(dataToSend)
+    }
+    const reset_form = () => {
+        cardNumber.value = ''
+        cardName.value = ''
+        cardMMYY.value = ''
+        cardCVC.value = ''
+    }
+</script>
+
+<style scoped>
+    .text-title {
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .first-section-container {
+        display: flex;
+        gap: 4rem;
+    }
+    .add-card-form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 1rem;
+    }
+
+    input {
+        border: 1px solid black;
+        border-radius: 5px;
+        font-size: 14px;
+        padding: 3px;
+        width: 350px;
+        height: 30px;
+        margin-bottom: 15px;
+    }
+
+    #add-card-button {
+        background-color: #3f143e;
+        color: #FFF;
+        width: 357px;
+        padding: 10px 0;
+        border-radius: 5px;
+        transition: background-color 0.3s;
+    }
+
+    #add-card-button:hover {
+        background-color: #533752;
+        cursor: pointer;
+    }
+</style>
