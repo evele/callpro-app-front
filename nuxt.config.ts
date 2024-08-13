d1c1f0418ce194313eef780040abd674166f333d
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-08-13 10:19:39 -0300

    updated nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,8 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
-  // typescript: {
-  //   typeCheck: true,
-  // }
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  }
 })
