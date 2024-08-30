2763d7c6f534173d64ffd5ec5020fc440bbad86b
Eric <inux2012@gmail.com>
2024-08-30 08:42:06 -0300

    fix active class

--- components/reusables/navbar.vue
+++ components/reusables/navbar.vue
@@ -2,7 +2,7 @@
     <aside class="menu flex flex-col">
       <div class="logo p-4">Aca el logo</div>
       <ul class="menu-list">
-        <li class="nav-link nav-link--active p-3"><a :class="is_active('dashboard')"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <li class="nav-link" :class="is_active('dashboard')"><a><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M11.3499 5.60962L6.34926 9.61012H2.34875V15.6109H6.34926L11.3499 19.6114V5.60962Z" stroke="#FFFFFF" stroke-width="2.00025" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M19.4209 5.53961C21.2959 7.41512 22.3492 9.95852 22.3492 12.6105C22.3492 15.2625 21.2959 17.8059 19.4209 19.6814M15.8905 9.07005C16.828 10.0078 17.3546 11.2795 17.3546 12.6055C17.3546 13.9315 16.828 15.2032 15.8905 16.1409" stroke="#FFFFFF" stroke-width="2.00025" stroke-linecap="round" stroke-linejoin="round"/>
 </svg><span>Dashboard</span></a></li>
@@ -58,7 +58,7 @@
 
   const currentRoute = router.currentRoute;
 
-  const is_active = (route:string) => currentRoute.value.name == route?"is-active": "" 
+  const is_active = (route:string) => currentRoute.value.name == route?"nav-link--active": "" 
   
 </script>
 <style>
@@ -77,6 +77,7 @@
   }
 
  .nav-link {
+    padding: 0.75rem;
     list-style-type: none;
     border-radius: 1.25rem;
     width: 172px;
