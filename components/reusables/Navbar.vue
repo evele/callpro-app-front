a786e479789cfc56fdca3766d946b7e101d695d4
Eric <inux2012@gmail.com>
2024-08-30 10:53:21 -0300

    svg component

--- components/reusables/Navbar.vue
+++ components/reusables/Navbar.vue
@@ -8,10 +8,7 @@
 </svg><span>Dashboard</span></a></li>
         <li class="nav-link"><a>Chat</a></li>
         <li class="nav-link"><a>Contacts</a></li>
-        <li class="nav-link"><a><svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11.3499 5.60962L6.34926 9.61012H2.34875V15.6109H6.34926L11.3499 19.6114V5.60962Z" stroke="#757575" stroke-width="2.00025" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M19.4209 5.53961C21.2959 7.41512 22.3492 9.95852 22.3492 12.6105C22.3492 15.2625 21.2959 17.8059 19.4209 19.6814M15.8905 9.07005C16.828 10.0078 17.3546 11.2795 17.3546 12.6055C17.3546 13.9315 16.828 15.2032 15.8905 16.1409" stroke="#757575" stroke-width="2.00025" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+        <li class="nav-link"><a><AudioSVG/>
 <span>Library</span></a></li>
         <li class="nav-link"><a>Billing</a></li>
 
@@ -53,6 +50,7 @@
   function logout() {
     authStore.logout()
   }
+  
 
   const router = useRouter();
 
@@ -111,4 +109,5 @@
     width: 100%;
     text-align: center;
   }
+
 </style>
