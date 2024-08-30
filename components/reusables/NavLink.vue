cab0bf34d8c1f11b102b5025c3f8ee2a566aa1f7
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-08-30 11:57:34 -0300

    Link component developed

---
+++ components/reusables/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <li class="nav-link" :class="is_active(route)">
+        <NuxtLink :to="route" :class="is_active(route)">
+            <component :is="svg" />
+            <span>{{ name }}</span>
+        </NuxtLink>
+    </li>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        route: { type: String, required: true },
+        name: { type: String, required: true },
+        svg: { type: Object, required: true },
+    });
+
+    const router = useRouter();
+    const currentRoute = router.currentRoute;
+    const is_active = (route: string) => currentRoute.value.name == route ? "nav-link--active" : ""; 
+</script>
--- components/reusables/Navbar.vue
+++ components/reusables/Navbar.vue
@@ -2,16 +2,7 @@
     <aside class="menu flex flex-col">
       <div class="logo p-4">Aca el logo</div>
       <ul class="menu-list">
-        <li class="nav-link" :class="is_active('dashboard')"><a><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11.3499 5.60962L6.34926 9.61012H2.34875V15.6109H6.34926L11.3499 19.6114V5.60962Z" stroke="#FFFFFF" stroke-width="2.00025" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M19.4209 5.53961C21.2959 7.41512 22.3492 9.95852 22.3492 12.6105C22.3492 15.2625 21.2959 17.8059 19.4209 19.6814M15.8905 9.07005C16.828 10.0078 17.3546 11.2795 17.3546 12.6055C17.3546 13.9315 16.828 15.2032 15.8905 16.1409" stroke="#FFFFFF" stroke-width="2.00025" stroke-linecap="round" stroke-linejoin="round"/>
-</svg><span>Dashboard</span></a></li>
-        <li class="nav-link"><a>Chat</a></li>
-        <li class="nav-link"><a>Contacts</a></li>
-        <li class="nav-link"><a><AudioSVG/>
-<span>Library</span></a></li>
-        <li class="nav-link"><a>Billing</a></li>
-
+        <NavLink v-for="link in LINKS" :key="link.name" :name="link.name" :route="link.route" :svg="link.svg" />
       </ul>
       <ul class="menu-list mt-8 mb-auto">
         <li>
@@ -45,19 +36,26 @@
 </template>
 <script setup lang="ts">
   import { useAuthStore } from "@/stores"
+  import AudioSVG from "@/components/svgs/AudioSVG.vue"
 
   const authStore = useAuthStore()
   function logout() {
     authStore.logout()
   }
   
+  type NavLink = {
+    name: string
+    route: string
+    svg: object
+  }
 
-  const router = useRouter();
-
-  const currentRoute = router.currentRoute;
-
-  const is_active = (route:string) => currentRoute.value.name == route?"nav-link--active": "" 
-  
+  const LINKS: NavLink[] = [
+    { name: "Dashboard", route: "dashboard", svg: AudioSVG },
+    { name: "Chat", route: "chat", svg: AudioSVG },
+    { name: "Contacts", route: "contacts", svg: AudioSVG },
+    { name: "Library", route: "#", svg: AudioSVG },
+    { name: "Billing", route: "billing", svg: AudioSVG },
+  ]
 </script>
 <style>
 
