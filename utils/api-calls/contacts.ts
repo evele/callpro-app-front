57f4dc86d6e34afe5abb4e1fea526c81bc17c919
Eric <inux2012@gmail.com>
2024-09-03 08:55:50 -0300

    move functions to contacts

--- utils/api-calls/contacts.ts
+++ utils/api-calls/contacts.ts
@@ -67,3 +67,36 @@ export async function saveContact(data: ContactToSaveData): Promise<SaveContactA
     const response = await fetchWrapper.post(SAVE_CONTACT_URL, data) 
     return response as SaveContactAPIResponse
 }
+
+/* ----- Download Contacts ----- */
+type data_string = {
+    group_id: string
+}
+
+export async function downloadContactsFile(data: data_string): Promise<null> {
+    try {
+        const response = await fetchWrapper.post(GET_DOWNLOAD_CONTACT_FILE, data) ;
+        const group_type = data.group_id;        
+        const date_string = date_time_to_string();
+        
+        if (response instanceof  Blob) {
+            const blob = response;
+            const filename = `${group_type}-${'contacts'}-${date_string}${'.csv'}`;            
+
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', filename);
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+        } else {
+            console.error('The response is not a Blob.');
+        }        
+    } catch (error) {
+        console.error('Error processing the file download:', error);
+    }
+    return null;// TODO me tiraba error, busque y encontre esta solucion    
+}
