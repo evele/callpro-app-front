f12c4504ecae7b63213f968707094f95290ced3b
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-09-12 12:58:34 -0300

    working on the table

--- components/contacts/ContactsTable.vue
+++ components/contacts/ContactsTable.vue
@@ -1,25 +1,40 @@
 <template>
-    <div class="flex justify-center items-center w-full table-container">
-        
+    <div class="w-full table-container rounded-7 py-4 pl-4 px-2">
+        <DataTable :value="dataToShow" tableStyle="min-width: 40rem" class="contacts-table w-full flex flex-col gap-18">
 
-        <DataTable :value="dataToShow" tableStyle="min-width: 50rem" class="contacts-table p-5 w-full rounded-7">
-            <header class="table-header flex justify-between items-center">
-                <input type="text">
-                <div class="flex">
-                    <Button class="table-action-btn flex items-center p-2 rounded-2" label="Filter">
-                        <template #icon>
-                            <FilterSVG />
-                        </template>
-                    </Button>
-                    <Button class="table-action-btn flex items-center p-2 rounded-2" label="Sort by">
-                        <template #icon>
-                            <SortBySVG />
-                        </template>
-                    </Button>
-                </div>
-            </header>
-            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></Column>
+            <template #header>
+                <header class="flex flex-col w-full gap-18">
+                    <div class="flex justify-between items-center w-full">
+                        <div class="search-input-container">
+                            <SearchSVG class="search-icon" />
+                            <InputText type="text" placeholder="Search by Name, Phone or Group" class="search-input rounded-8" />
+                        </div>
+
+                        <div class="flex gap-4">
+                            <Button class="table-action-btn flex items-center py-2 px-3 rounded-9 gap-6" label="Filter">
+                                <template #icon>
+                                    <FilterSVG class="table-action-icon" />
+                                </template>
+                            </Button>
+                            <Button class="table-action-btn flex items-center py-2 px-3 rounded-9 gap-6" label="Sort by">
+                                <template #icon>
+                                    <SortBySVG class="table-action-icon" />
+                                </template>
+                            </Button>
+                        </div>
+                    </div>
+                    
+                    <div class="flex items-center gap-4 selected-group-info rounded-4">
+                        <CheckSVG class="check-icon rounded-full" />
+                        <span>All</span>
+                    </div>
+                </header>
+            </template>
+
+            <Column selectionMode="multiple" headerStyle="text-align: left"></Column>
+            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"
+                :class="{'left-aligned-column': col.field === 'name', 'center-aligned-column': col.field !== 'name'}"
+            ></Column>
         </DataTable>
     </div>
     
@@ -47,27 +62,66 @@
     .table-container {
         max-width: 850px;
         max-height: 780px;
-    }
-
-    .contacts-table {
         background-color: white;
-        font-size: 14px;
         box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    }
-
-    .table-header {
-
+        font-size: 14px;
     }
     
     .table-action-btn {
         background-color: transparent;
         border: none;
-        gap: 6px;
         transition: background-color 0.3s;
+        font-weight: 600;
 
         &:hover {
             cursor: pointer;
             background-color: #F5F5F5;
         }
     }
+
+    .table-action-icon {
+        color: #757575;
+    }
+
+    .search-input-container {
+        position: relative;
+        display: inline-block;
+    }
+
+    .search-icon {
+        position: absolute;
+        left: 10px; 
+        top: 50%;
+        transform: translateY(-50%);
+        pointer-events: none;
+    }
+
+    .search-input {
+        border: 1px solid #DED8E1;
+        padding: 2px 10px 2px 35px;
+        width: 280px;
+        height: 38px;
+    }
+
+    .selected-group-info {
+        background-color: #e1daf0;
+        padding: 4px 8px;
+        border-bottom: 1px solid #ccc;
+        width: fit-content;
+        color: black;
+    }
+
+    .check-icon {
+        background-color: #65558F;
+        color: white;
+        width: 17.143px;
+        height: 17.143px;
+        padding: 1px;
+    }
+    
+
+    // .contacts-table td {
+    //     text-align: center;
+    //     vertical-align: middle;
+    // }
 </style>
