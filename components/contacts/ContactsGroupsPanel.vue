a4fe4c1a61bca8579a0ab6b9d504de8feba7ae87
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-09-19 12:10:27 -0300

    panel developed

--- components/contacts/ContactsGroupsPanel.vue
+++ components/contacts/ContactsGroupsPanel.vue
@@ -1,21 +1,47 @@
 <template>
     <nav class="container flex flex-col">
-        <div>
+        <div class="first-section">
             <h4 class="groups-title">Groups</h4>
 
             <ul class="flex flex-col default-groups-ul">
-                
+                <li class="fake-button"></li>
+                <li class="fake-button"></li>
+                <li class="fake-button"></li>
             </ul>
         </div>
 
-        <div>
+        <Divider class="divider" />
 
+        <div class="second-section flex flex-col">
+            <div class="fake-button"></div>
+            <ul class="user-group-container flex flex-col">
+                <li class="flex justify-end" v-for="group in USER_GROUPS" :key="group.number">
+                    <Button class="user-group-btn flex justify-between items-center" :class="group.bgColor">
+                        <div class="flex items-center user-group-data">
+                            <span class="user-group-name">{{ group.name }}</span>
+                            <span class="user-group-number">#{{ group.number }}</span>
+                        </div>
+                        <EditIconSVG />
+                    </Button>
+                </li>
+            </ul>
         </div>
+
+        <Button class="add-new-btn mx-auto">
+            <PlusSVG class="plus-icon" />
+            <span class="add-new-text">Add new</span>
+        </Button>
     </nav>
 </template>
 
 <script setup lang="ts">
     import EditIconSVG from "@/components/svgs/EditIconSVG.vue"
+
+    const USER_GROUPS = [
+        { name: "Group 1", number: "0054", bgColor: "bg-green" },
+        { name: "Group 2", number: "0044", bgColor: "bg-yellow" },
+        { name: "Group 3", number: "0034", bgColor: "bg-red" },
+    ]
 </script>
 
 <style scoped lang="scss">
@@ -27,7 +53,21 @@
         overflow: hidden;
         background-color: #FFF;
         gap: 12px;
-        padding: 28px 16px;
+        padding: 28px 0 0 0;
+
+        .first-section {
+            padding: 0 16px;
+        }
+
+        .divider {
+            background: #CAC4D0;
+            height: 0.5px;
+        }
+
+        .second-section {
+            padding: 0 16px;
+            gap: 10px;
+        }
     }
 
     .groups-title {
@@ -38,5 +78,105 @@
     
     .default-groups-ul {
         gap: 6px;
+        list-style: none;
+        padding: 0;
+        text-align: end;
+    }
+
+    .fake-button {
+        width: 100%;
+        height: 50px;
+        border-radius: 10px;
+        background-color: #F4F0EF;
+    }
+
+    .user-group-container {
+        max-height: 94px;
+        overflow-y: scroll;
+        gap: 5px;
+        padding: 0;
+    }
+
+    .user-group-btn {
+        width: 94%;
+        border-radius: 10px;
+        height: 35px;
+        border: none;
+        padding: 10px 12px;
+        gap: 8px;
+
+        .user-group-data {
+            gap: 4px;
+
+            .user-group-name {
+                font-size: 14px;
+                font-weight: 500;
+                line-height: 140%;
+                color: #1D192B;
+            }
+
+            .user-group-number {
+                color: #79747E;
+                font-size: 12px;
+                font-style: italic;
+                line-height: 140%;
+            }
+        }
+    }
+
+    .bg-green {
+        background-color: #EBFFEE;
+    }
+    .bg-yellow {
+        background-color: #FFFBEB;
+    }
+    .bg-red {
+        background-color: #FEE9E7;
+    }
+
+    .add-new-btn {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        color: #FFF;
+        width: 180px;
+        height: 45px;
+        padding: 10px 0;
+        margin-top: 10px;
+        gap: 8px;
+        border-radius: 18px;
+        background-color: #1D192B;
+        box-shadow: 0px 0.9px 1.8px 0px rgba(0, 0, 0, 0.30), 0px 1.8px 5.4px 1.8px rgba(0, 0, 0, 0.15);
+
+        &:hover {
+            cursor: pointer;
+        }
+
+        .plus-icon {
+            position: absolute;
+            left: 40%; 
+            top: 50%;
+            transform: translateY(-50%);
+            transition: left 0.3s ease;
+        }
+
+        .add-new-text {
+            position: absolute;
+            min-width: 80px;
+            left: 50%;
+            opacity: 0;
+            transition: left 0.3s ease, opacity 0.3s ease;
+        }
+
+        &:hover .plus-icon {
+            left: 20%;
+        }
+
+        &:hover .add-new-text {
+            left: 37%;
+            opacity: 1;
+        }
     }
 </style>
