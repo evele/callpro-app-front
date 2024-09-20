8f6673c1d6109d3fad02fc864c87394fd11992e6
GrossoMA <grossom@gmail.com>
2024-09-20 11:09:41 -0300

    custom y prime button

---
+++ components/newcontacts/CustomButton.vue
@@ -0,0 +1,71 @@
+<template lang="">
+    <div class="custom-button"    :class="{ 'active': active }"     @click="handleClick">    
+    <slot name="icon"></slot> 
+    <div class="content">
+      <span class="text">{{ text }}</span>
+      <span class="value">{{ value }}</span>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: [String, Number],
+        required: true
+    }, 
+    active: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(['click']);
+const handleClick = () => {
+  emit('click');
+};
+
+</script>
+<style scoped>
+.custom-button {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 12px;
+  cursor: pointer;
+  border-radius: 10px;
+  background: var(--Schemes-Surface-Tint, #FFF);
+  color: var(--Schemes-On-Primary, #6750A4);
+  transition: background 0.3s ease; 
+}
+
+.custom-button.active {
+  background: var(--Schemes-On-Primary, #6750A4);
+  color: var(--Schemes-Surface-Tint, #FFF); 
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+}
+
+.text {
+  margin-right: 5px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+}
+
+.value {
+  font-family: 'Roboto', sans-serif;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 15px;
+  color: var(--Schemes-Surface-Container-Low, #1c1b1d);
+}
+</style>
---
+++ pages/newcontacts.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="button-page">
+        <!-- botones dinamicos -->
+        <CustomButton v-for="(button, index) in buttons" :key="index" :text="button.text" :value="button.value"
+            :class="{ 'styled-button': true }" :active="activeButtonIndex === index" @click="setActiveButton(index)">
+            <!-- Renderiza el componente SVG correspondiente -->
+            <template #icon>
+                <component :is="button.icon" />
+            </template>
+        </CustomButton>
+    </div>
+</template>
+<script setup lang="ts">
+import ContactsSVG from "@/components/svgs/ContactsSVG.vue"
+import GroupsSVG from "@/components/svgs/GroupsSVG.vue"
+import DncSVG from "@/components/svgs/DncSVG.vue"
+import UploadSVG from "@/components/svgs/UploadSVG.vue"
+
+const buttons = [
+    { text: 'ALL', value: 0, icon: ContactsSVG },// sumo los ramdomssss
+    { text: 'Unassigned', value: Math.floor(Math.random() * 100), icon: GroupsSVG },//ramdom para los valores
+    { text: 'Monthly Number', value: Math.floor(Math.random() * 100), icon: DncSVG },
+    { text: 'Trash', value: Math.floor(Math.random() * 100), icon: UploadSVG }
+];
+buttons[0].value = buttons.slice(1).reduce((acc, button) => acc + button.value, 0);
+
+const activeButtonIndex = ref<number | null>(0);
+
+const setActiveButton = (index: number) => {
+  activeButtonIndex.value = index;
+};
+
+</script>
+
+<style scoped>
+.button-page {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+.styled-button.active {
+  background-color: var(--Schemes-On-Primary, #6750A4);
+  color: var(--Schemes-Surface-Tint, #FFF);
+}
+
+.styled-button :deep(.custom-button) {
+  background-color: var(--Schemes-Surface-Tint, #6750A4);
+}
+
+.styled-button.active :deep(.custom-button) {
+  background-color: var(--Schemes-On-Primary, #FFF);
+}
+</style>
