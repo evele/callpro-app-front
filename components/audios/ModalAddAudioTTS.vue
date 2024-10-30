72f908a25acd07340aeb3b9d704a77873a3683d5
GrossoMA <grossom@gmail.com>
2024-10-30 09:10:13 -0300

    save to change branch

---
+++ components/audios/ModalAddAudioTTS.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- TODO: text 2xl 3xl dont' function -->
+    <Dialog v-model:visible="visible" modal header="Add Audio from TTS" class="text-2xl w-full max-w-[850px]">
+        <Divider class="max-w-[850px]" />
+
+        <!-- Agrupamos en un contenedor con flex y gap-->
+        <div class="flex flex-col gap-[38px] mt-9">
+
+            <div className="w-[738px] h-[33px] text-[#1e1e1e] text-sm font-normal font-['Inter'] leading-snug">Type the
+                message here to have it converted to audio.</div>
+
+        </div>
+        <div class="modal">
+            <button @click="$emit('close')">Cerrar</button>
+            <!-- Contenido del modal -->
+        </div>
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import ChevronDownSVG from '../svgs/ChevronDownSVG.vue';
+
+const visible = ref(false);
+
+const open = () => {
+    visible.value = true;
+}
+
+const close = () => {
+    visible.value = false;
+}
+
+</script>
+<style scoped></style>
--- pages/library.vue
+++ pages/library.vue
@@ -4,6 +4,23 @@
             <PageHeader text="Library"/>
         </template>
         <template #content>
+            <Button class="add-new-btn mx-auto" @click="open_modal">            
+            <span class="add-new-text">Modal Add Audio TTS</span>
+            <ModalAddAudioTTS v-if="isModalOpen" @close="close_modal"/>
+        </Button>
         </template>
    </Page>
 </template>
+<script setup lang="ts">
+
+const isModalOpen = ref(false);
+
+const open_modal = () => {
+    isModalOpen.value = true;
+};
+
+const close_modal = () => {
+    isModalOpen.value = false;
+};
+
+</script>
