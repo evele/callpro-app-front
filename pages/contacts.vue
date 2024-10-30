de0ba11750f7ede65851b09b223ac59b4e47fe75
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-10-30 10:20:59 -0300

    refactor of  contacts panel

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <p class="text-title">Contact page</p>
+        <p class="text-center text-2xl text-bold">Contact page</p>
     </div>
     <div class="py-5 main-container">
         <ContactsTable :selected-tab="selected_tab" />
-        <div>
+        <div class="flex flex-col gap-4">
             <ContactsActions />
             <ContactsGroupsPanel />
         </div>
@@ -27,79 +27,11 @@ const open_new_contact_modal = () => {
 </script>
 
 <style scoped>
-.text-title {
-    text-align: center;
-    font-size: 24px;
-    font-weight: bold;
-}
-
-.tab-style {
-    display: flex;
-    list-style-type: none;
-    padding: 0;
-    gap: 1rem;
-    margin-left: 1rem;
-}
-
-.tab-style__li {
-    color: gray;
-    background-color: transparent;
-    transition: background-color 0.3s;
-    font-weight: bold;
-    border: 1px solid gray;
-    padding: 6px 1rem;
-    font-size: 14px;
-}
-
-.tab-style__li:hover {
-    cursor: pointer;
-    color: white;
-    background-color: gray;
-}
-
-.selected-tab {
-    color: white;
-    background-color: orange;
-}
-
-.filter-container {
-    display: flex;
-    gap: 1.4rem;
-    margin: 1rem 0 0 1rem;
-}
-
-.contact-list {
-    list-style-type: none;
-    padding: 0;
-    margin: 1rem 0;
-    border: 1px solid #ccc;
-    border-collapse: collapse;
-}
-
-.contact-item {
-    display: flex;
-    padding: 10px;
-    border-bottom: 1px solid #ccc;
-}
-
-.contact-item:last-child {
-    border-bottom: none;
-}
-
-.contact-label {
-    font-weight: 600;
-    margin-right: 6px;
-}
-
-.contact-value {
-    margin-right: 10px;
-    color: blue;
-}
-
 .main-container {
     background-color: var(--body-background);
     overflow-x: hidden;
     display: grid;
     grid-template-columns: 1fr auto;
+    padding: 0 30px;
 }
 </style>
