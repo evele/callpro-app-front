6f028ded83b6607feabbec7452a9cb1d0b3d004c
compu <inux2012@gmail.com>
2024-11-29 12:56:01 -0300

    some fixes

--- components/audios/ModalCallInCodes.vue
+++ components/audios/ModalCallInCodes.vue
@@ -6,7 +6,6 @@
             </h2>
         </template>
 
-        <!-- Botones Create - ADD -->
         <div class="flex flex-col gap-12 px-0 sm:flex-row sm:px-28">
             <Button type="button" @click="create_call_in_code('1')"
                 class="w-full bg-white text-black rounded-xl shadow text-sm border-none hover:bg-black hover:text-white">
@@ -18,7 +17,6 @@
             </Button>
         </div>
 
-        <!-- Contenedor de tabla -->
         <div class="w-full max-h-[314px] overflow-y-auto mt-6 mb-10   rounded-tl-xl rounded-tr-xl">
             <p v-if="isLoading" class="text-center my-4 text-gray-500">Loading data...</p>
             <p v-else-if="isError" class="text-center my-4 text-red-500">Error: {{ errorMessage }}</p>
@@ -26,30 +24,31 @@
                 <table v-if="userCallInCodes && 'user_call_in_codes' in userCallInCodes" class="w-full text-gray-700 border border-gray-200 ">
                     <thead>
                         <tr class="bg-[#653494] h-[38px] ">
-                            <th
-                                class="w-[220px] px-8 text-center text-white text-sm font-medium font-['Roboto'] tracking-tight">
+                            <th class="px-4 text-center text-white text-sm font-medium w-24">
+                                One Time
+                            </th>
+                            <th class="px-4 text-center text-white text-sm font-medium">
                                 Call in codes
                             </th>
-                            <th
-                                class="w-[220px] px-8 text-center text-white text-sm font-medium font-['Roboto'] tracking-tight">
+                            <th class="px-4 text-center text-white text-sm font-medium">
                                 Date Created
                             </th>
-                            <th
-                                class="w-[60px] px-8 text-center text-white text-sm font-medium font-['Roboto'] tracking-tight">
+                            <th class="px-8 text-center text-white text-sm font-medium w-24">
                             </th>
                         </tr>
                     </thead>
                     <tbody  class="divide-y divide-gray-200">
                         <tr v-for="(code, index) in userCallInCodes.user_call_in_codes" :key="code.id" 
                             :class="[index % 2 === 0 ? 'bg-gray-100' : 'bg-white','hover:bg-[#9884cf]']" 
-                            class="pt-4 pb-[15px] h-[69px]">
-                            <td class="w-[220px] text-center py-2">{{ code.call_in_code }}</td>
-                            <td class="w-[220px] text-center py-2">{{ code.date }}</td>
-                            <td class="w-[60px] py-2">
+                            class="pt-4 pb-[15px] h-16">
+                            <td class="text-center w-24"><CheckSVG v-if="code.is_static=='0'" class="m-auto w-6 h-6"></CheckSVG></td>
+                            <td class="text-center">{{ code.call_in_code }}</td>
+                            <td class="text-center">{{ code.date }}</td>
+                            <td class="text-center w-24">
                                 <Button
                                     @click="delete_code(code?.id)"
                                     class="bg-gray-200 py-1 px-[6px] border-none text-black hover:bg-[#9884cf] hover:text-white">
-                                    <TrashSVG class="w-4 h-4" />
+                                    <TrashSVG class="w-6 h-6" />
                                 </Button>
                             </td>
                         </tr>
@@ -73,6 +72,7 @@
     </Dialog>
 </template>
 <script setup lang="ts">
+
 const { data: userCallInCodes, isError, error: errorMessage, isLoading, isSuccess, refetch } = useFetchCallInCodes()
 const { mutate: createCallInCode } = useCreateCallInCode()
 const { mutate: deleteCallInCode } = useDeleteCallInCode()
@@ -93,8 +93,6 @@ const delete_code = (id: number) => {
 
 const open = () => {
     visible.value = true;
-    // const body = document.body;
-    // body.style.overflow = 'hidden';
 }
 
 const close = () => {       
