cfd2d648d19ff1878f784594179d70eaca092fc8
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-12-02 11:55:02 -0300

    working on save audio converted

--- components/library/ModalAddAudioTTS.vue
+++ components/library/ModalAddAudioTTS.vue
@@ -1,8 +1,14 @@
 <template>
-    <Dialog v-model:visible="visible" modal header="Add Audio from TTS" @hide="close"
-        class="text-2xl w-full max-w-[850px] gap-[38px]">
-        <!--  -->
-        <div class="flex flex-col  gap-[38px]">
+    <Dialog v-model:visible="visible" modal :draggable="false" :closable="false" class="text-2xl w-full max-w-[850px]">
+        <template #header>
+            <header class="w-full flex justify-between pb-5 px-8">
+                <h2 class="flex items-center gap-4 font-bold text-lg text-black">Add Audio from TTS <ChevronDownSVG /></h2>
+                <Button @click="close" class="bg-transparent border-none text-black hover:bg-gray-200"><CloseSVG /></Button>
+            </header>
+            <Divider class="absolute left-0 top-[75px]" />
+        </template>
+
+        <div class="flex flex-col gap-[38px]">
             <InfoPanel>Type
                 the message here to have it converted to audio.
             </InfoPanel>  
@@ -17,7 +23,7 @@
                         {{ isConverting ? 'Converting...' : 'Convert' }}
                     </button>                
             </div>
-            <ul v-if="isSuccess && convertedAudios.length" class="ml-2 is-flex is-flex-direction-column is-gap-1">
+            <ul v-if="convertedAudios?.length" class="ml-2 is-flex is-flex-direction-column is-gap-1">
 
                 <li v-for="(audio, index) in convertedAudios" :key="index" class="is-flex is-gap-2">
                     <!-- area preview aduio conver -->
@@ -59,41 +65,43 @@
                             </div>
                             <!-- download -->
                             <div class="w-6 h-6 bg-[#e7e0ec] rounded-[10px] justify-center items-center flex">
-                                <DownloadSVG class="w-6 h-6 relative bg-[#e7e0ec] rounded-[10px]" />
+                                <DownloadSVG class="w-5 h-5 relative bg-[#e7e0ec] rounded-[10px]" />
                             </div>
                             <!-- edit -->
-                            <div @click="startEditing(index, audio.file_name)"
-                                class="w-6 h-6 p-1 bg-[#e7e0ec] rounded-[10px] justify-center items-center flex">
-                                <EditIconSVG class="w-6 h-6 relative bg-[#e7e0ec] rounded-[10px]" />
-                            </div>
+                            <Button @click="startEditing(index, audio.file_name)" class="w-6 h-6 bg-[#e7e0ec] rounded-[10px] text-[#1D1B20] border-none hover:scale-110 transition-transform">
+                                <template #icon>
+                                    <EditIconSVG class="w-4 h-4 relative bg-[#e7e0ec] rounded-[10px]" />
+                                </template>
+                            </Button>
                             <!-- trash -->
-                            <div class="w-6 h-6  bg-[#e7e0ec] rounded-[10px] justify-center items-center flex">
-                                <TrashSVG class="w-6 h-6 relative bg-[#e7e0ec] rounded-[10px] " />
-                            </div>
-                        </div><!-- button action -->
+                            <Button @click="handle_delete(audio.file_name)" class="w-6 h-6 bg-[#e7e0ec] rounded-[10px] text-[#1D1B20] border-none hover:scale-110 transition-transform">
+                                <template #icon>
+                                    <TrashSVG class="w-5 h-5 relative bg-[#e7e0ec] rounded-[10px]" />
+                                </template>
+                            </Button>
+                        </div>
 
                     </div>
                 </li>
             </ul>
 
         </div>
-        <!--  -->
+
         <template #footer>
-            <Button type="button" class="justify-center items-center w-[300px] mx-auto" :disabled="isPending || !isSaveEnabled">
-                {{ isPending ? 'Saving...' : 'Save' }}
+            <Button @click="save_audios" type="button" class="justify-center items-center w-[300px] mx-auto mt-[38px]" :disabled="disabled_save_btn">
+                {{ isPendingSave ? 'Saving...' : 'Save' }}
             </Button>
         </template>
     </Dialog>
 </template>
 
 <script setup lang="ts">
-
-import InfoPanel from '../reusables/InfoPanel.vue';
-import CloseSVG from '../svgs/CloseSVG.vue';
-import DownloadSVG from '../svgs/DownloadSVG.vue';
-import EditIconSVG from '../svgs/EditIconSVG.vue';
-import PlaySVG from '../svgs/PlaySVG.vue';
-import TrashSVG from '../svgs/TrashSVG.vue';
+import InfoPanel from '~/components/reusables/InfoPanel.vue';
+import CloseSVG from '~/components/svgs/CloseSVG.vue';
+import DownloadSVG from '~/components/svgs/DownloadSVG.vue';
+import EditIconSVG from '~/components/svgs/EditIconSVG.vue';
+import PlaySVG from '~/components/svgs/PlaySVG.vue';
+import TrashSVG from '~/components/svgs/TrashSVG.vue';
 
 const audio_id: Ref<string | null> = ref(null);
 const audio_url: Ref<string | null> = ref(null);
@@ -104,7 +112,6 @@ const isPending = ref(false);
 const show_older = ref(false)
 // TODO: audios convertidos pensnado en que sean mas de uno...
 const convertedAudios = ref<Tts_Convert[]>([]);
-    const isSaveEnabled = computed(() => convertedAudios.value.length > 0);
 // Variables audio Name edit
 interface AudioFile {
     file_name: string;
@@ -113,11 +120,9 @@ const editingIndex = ref<number | null>(null); // indice del audio en edición
 const audioNameTemp = ref("");  // temporal para el nombre del audio
 let originalExtension = ""; // para almacenar la extension original del archivo
 
-
-const { data: allAudiosData, isLoading: loadingAllAudios, isSuccess, refetch } = useFetchGetAllAudios(show_older)
 const { mutate: createTextToSpeech, isPending: isConverting } = useConvertTextToSpeech()
 const { data: audioData, refetch: refetchAudioData } = useFetchGetAudio(audio_id, audio_url, CALLPRO_APP_FRONT)
-
+const { mutate: saveAudio, isPending: isPendingSave } = useSaveAudio()
 
 const close = () => {
     visible.value = false;
@@ -129,6 +134,7 @@ const close = () => {
     audioNameTemp.value = "";
 };
 
+const disabled_save_btn = computed(() => !convertedAudios.value.length || isPendingSave.value || isConverting.value)
 
 const convert_Text = async () => {
     audio_id.value = null
@@ -145,9 +151,9 @@ const convert_Text = async () => {
 
     createTextToSpeech(dataToSend, {
         onSuccess: (data: Tts_Convert) => {
-            console.log("paso intermedio");
             audio_id.value = PREVIEW_TTS;
             audio_url.value = data.full_file_url;
+            text_to_convert.value = "";
 
             // Agrego el nuevo audio al array
             convertedAudios.value.push({
@@ -156,7 +162,6 @@ const convert_Text = async () => {
             });
         }
     });
-    console.log("--->", convertedAudios)
 }
 
 // Función para iniciar edición
@@ -173,6 +178,8 @@ const startEditing = (index: number, currentName: string) => {
 
     audioNameTemp.value = splitName.join("."); // Almacena solo el nombre sin la extensión
 }
+
+const handle_delete = (fileName: string) => convertedAudios.value = convertedAudios.value.filter((audio: Tts_Convert) => audio.file_name !== fileName);
 
 // Función para guardar el nombre editado
 const saveAudioName = (index: number) => {
@@ -194,10 +201,41 @@ const cancelEditing = () => {
     audioNameTemp.value = ''; // Limpia el nombre temporal si es necesario
 }
 
-const open = () => {
-    visible.value = true;
-    console.log("open")
+const { show_success_toast, show_error_toast } = usePrimeVueToast();
+
+const save_audios = () => {
+    if(!convertedAudios.value.length) return
+    
+    const audios_to_save: AudioInfoToSave[] = convertedAudios.value.map((audio: Tts_Convert) => {
+        return {
+            audio_id: audio_id.value,
+            file_name: audio.file_name,
+            length: 0,
+            name: audio.file_name
+        }
+    })
+
+    const data_to_send: AudioToSave = {
+        action: 'create',
+        audio_info: audios_to_save
+    }
+    console.log(data_to_send)
+    return
+    saveAudio(data_to_send, {
+        onSuccess: (response: APIResponseSuccess | APIResponseError) => {
+            if(response.result) {
+                show_success_toast('Success', 'Data saved successfully!')
+                close()
+            } else {
+                show_error_toast('Oops...', response.error ?? 'Something failed!')
+            }
+        },
+        onError: () => show_error_toast('Oops...', 'Something failed!')
+    })
 }
+
+const open = () => visible.value = true;
+
 defineExpose({ open });
 
 </script>
--- pages/audios.vue
+++ pages/audios.vue
@@ -2,62 +2,21 @@
     <div class="p-6">       
         <Card class="bg-white">
             <template #content>
-                <ButtonsLibraryContainer />
+                <ButtonsLibraryContainer @action="handle_btn_action" />
                 <AudiosTable />
             </template>  
         </Card>
-
-        <div>
-            <h3>Write some text and convert it to speech.</h3>
-            <textarea cols="50" rows="10" v-model="text_to_convert" />
-            <button type="button" @click="convert_Text" class="btn-convert" :disabled="isConverting">
-                {{ isConverting ? 'Converting...' : 'Convert' }}
-            </button>
-        </div>
-        <div v-if="audio_id">
-            <h4>Preview the converted file</h4>
-            <button type="button" @click="fetch_audio_data" :disabled="isLoading">
-                {{ isLoading ? 'Loading...' : 'Load audio' }}
-            </button>
-        </div>
     </div>
+    <ModalAddAudioTTS ref="addAudioTTSRef"/>
 </template>
 
 <script setup lang="ts">
-    const text_to_convert = ref('')
-    const isLoading = ref(false)
-    
-    const audio_id: Ref<string | null> = ref(null);
-    const audio_url: Ref<string | null> = ref(null);    
-    
-    const { mutate: createTextToSpeech, isPending: isConverting } = useConvertTextToSpeech()
-    const { data: audioData, refetch: refetchAudioData } = useFetchGetAudio(audio_id, audio_url, CALLPRO_APP_FRONT)
+    const addAudioTTSRef = ref(null)  
 
-    const convert_Text = async () => {
-        audio_id.value = null
-        audio_url.value = null
-
-        if(text_to_convert.value.trim() === '') {
-            alert('Please write some text to convert.')
-            return
+    const handle_btn_action = (action: string) => {
+        if (action === 'tts') {
+            addAudioTTSRef.value.open()
         }
-
-        const dataToSend = {
-            text: text_to_convert.value.trim(),
-            temp: false
-        }
-
-        createTextToSpeech(dataToSend, {
-            onSuccess: (data:Tts_Convert) => {                
-                audio_id.value = PREVIEW_TTS
-                audio_url.value = data.full_file_url
-            }
-        })
-        
-    }
-
-    const fetch_audio_data = () => {
-        refetchAudioData()
     }
 </script>
 
