6c00a4a463a768ff353e60dc3baa7a5f5f29abac
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-12-04 10:37:24 -0300

    header component developed

---
+++ components/svgs/ShoppingCartSVG.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
+        <g clip-path="url(#clip0_2253_14966)">
+            <path d="M6.85156 16.9004C7.26578 16.9004 7.60156 16.5646 7.60156 16.1504C7.60156 15.7362 7.26578 15.4004 6.85156 15.4004C6.43735 15.4004 6.10156 15.7362 6.10156 16.1504C6.10156 16.5646 6.43735 16.9004 6.85156 16.9004Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M15.1016 16.9004C15.5158 16.9004 15.8516 16.5646 15.8516 16.1504C15.8516 15.7362 15.5158 15.4004 15.1016 15.4004C14.6873 15.4004 14.3516 15.7362 14.3516 16.1504C14.3516 16.5646 14.6873 16.9004 15.1016 16.9004Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M0.851562 1.15039H3.85156L5.86156 11.1929C5.93015 11.5382 6.11799 11.8484 6.39222 12.0691C6.66645 12.2899 7.00959 12.4071 7.36156 12.4004H14.6516C15.0035 12.4071 15.3467 12.2899 15.6209 12.0691C15.8951 11.8484 16.083 11.5382 16.1516 11.1929L17.3516 4.90039H4.60156" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        <defs>
+            <clipPath id="clip0_2253_14966">
+                <rect width="18" height="18" fill="currentColor" transform="translate(0.101562 0.400391)"/>
+            </clipPath>
+        </defs>
+    </svg>
+</template>
