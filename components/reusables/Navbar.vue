0357dfbecd68beca749a162eebc77bcfd706d1b7
compu <inux2012@gmail.com>
2024-12-26 13:30:19 -0300

    remove fixed

--- components/reusables/Navbar.vue
+++ components/reusables/Navbar.vue
@@ -1,20 +1,22 @@
 <template>
-  <aside class="flex flex-col w-[250px] border border-[#DED8E1] justify-between pt-14 fixed top-0 left-0 h-screen z-[1000]">
-    <div class="flex flex-col items-center gap-14">
-      <NuxtLink to="dashboard">
-        <img src="@/assets/png/thecallpro.png" alt="The CallPro Logo"  class="w-[120px] h-9 mx-auto" />
-      </NuxtLink>
-      <ul class="flex flex-col p-0 items-center gap-[18px]">
-        <NavLink v-for="link in LINKS" :key="link.name" :name="link.name" :route="link.route" :icon="link.icon" />
-      </ul>
-    </div>
-
-    <div class="p-4">
-      <Divider class="!h-[2.5px] bg-[#DED8E1] rounded-full"/>
-      <ul class="flex flex-col p-0 items-center gap-4">
-        <NavLink name="Settings" :icon="SettingsSVG" />
-        <NavLink name="Logout" :icon="LogoutSVG" />
-      </ul>
+  <aside>
+    <div class="flex flex-col w-[250px] border border-[#DED8E1] justify-between pt-14 sticky top-0 left-0 h-screen z-[1000]">
+      <div class="flex flex-col items-center gap-14">
+        <NuxtLink to="dashboard">
+          <img src="@/assets/png/thecallpro.png" alt="The CallPro Logo"  class="w-[120px] h-9 mx-auto" />
+        </NuxtLink>
+        <ul class="flex flex-col p-0 items-center gap-[18px]">
+          <NavLink v-for="link in LINKS" :key="link.name" :name="link.name" :route="link.route" :icon="link.icon" />
+        </ul>
+      </div>
+      
+      <div class="p-4">
+        <Divider class="!h-[2.5px] bg-[#DED8E1] rounded-full"/>
+        <ul class="flex flex-col p-0 items-center gap-4">
+          <NavLink name="Settings" :icon="SettingsSVG" />
+          <NavLink name="Logout" :icon="LogoutSVG" />
+        </ul>
+      </div>
     </div>
 </aside>
 </template>
