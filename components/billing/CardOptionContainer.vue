a80ee3b5f7eafbe14dcfb6dc40494acd31be19bf
compu <inux2012@gmail.com>
2025-02-17 10:40:21 -0300

    remove computed

--- components/billing/CardOptionContainer.vue
+++ components/billing/CardOptionContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-[196px] flex flex-col justify-between rounded-xl p-3 pb-5" :class="theme">
+    <div class="h-[196px] flex flex-col justify-between rounded-xl p-3 pb-5" :class="type">
         <slot name="chip-icon" />
         <slot name="card-content" />
     </div>
@@ -9,20 +9,18 @@
     const props = defineProps<{
         type: SelectedBillingType
     }>()
-
-    const theme = computed(() => {
-        if(props.type === 'credit') return 'w-[188px] credit-bg text-white'
-        return 'w-[146px] plan-bg'
-    })
 </script>
 
 <style scoped lang="scss">
-    .credit-bg {
+    .credit {
         border: 0.3px solid #CAC4D0;
         background: radial-gradient(397.76% 355.04% at -223.94% 227.3%, #1F102E 0%, #653494 48%, #1F102E 100%);
+        width: 188px; /* TODO: maybe we will handle the width from a grid container */
+        color: #FFF;
     }
-    .plan-bg {
+    .plan {
         border: 0.3px solid rgba(101, 52, 148, 0.60);
         background: radial-gradient(273.33% 318.02% at -68.84% -32.65%, #9747FF 0%, #F7F2FA 50.75%, #F7F2FA 100%);
+        width: 146px;
     }
 </style>
