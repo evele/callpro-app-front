78dcd71ff60dd497f3c357a364af360fc45b6bd4
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2025-03-31 09:03:10 -0300

    added gray overlay to AudioPlayer component

--- components/reusables/Navbar.vue
+++ components/reusables/Navbar.vue
@@ -2,30 +2,30 @@
   <aside>
     <div 
       class="flex flex-col border border-[#DED8E1] justify-between pt-14 sticky top-0 h-screen z-[1000] transition-all duration-300 ease-in-out"
-      :class="[collapse ? 'w-[116px]' : 'w-[250px]']"  
+      :class="[is_navbar_collapsed ? 'w-[116px]' : 'w-[250px]']"  
     >
       <div class="flex flex-col items-center gap-14">
-        <div class="flex items-center w-full px-4" :class="[collapse ? 'justify-center gap-1' : 'justify-end gap-10']">
+        <div class="flex items-center w-full px-4" :class="[is_navbar_collapsed ? 'justify-center gap-1' : 'justify-end gap-10']">
           <Transition name="slide-fade">
-            <NuxtLink v-show="!collapse" to="dashboard">
+            <NuxtLink v-show="!is_navbar_collapsed" to="dashboard">
               <img src="@/assets/png/thecallpro.png" alt="The CallPro Logo"  class="w-[120px] h-9 mx-auto" />
             </NuxtLink>
           </Transition>
-          <div v-if="collapse" class="h-9"></div>
+          <div v-if="is_navbar_collapsed" class="h-9"></div>
 
-          <ExpandSVG v-if="collapse" class="min-w-5 min-h-5 text-dark-5 cursor-pointer hover:scale-110 transition-transform" @click="toggle_collapse" />
-          <CollapseSVG v-else class="min-w-5 min-h-5 text-dark-5 cursor-pointer hover:scale-110 transition-transform" @click="toggle_collapse" />
+          <ExpandSVG v-if="is_navbar_collapsed" class="min-w-5 min-h-5 text-dark-5 cursor-pointer hover:scale-110 transition-transform" @click="generalStore.toggleCollapseNavbar" />
+          <CollapseSVG v-else class="min-w-5 min-h-5 text-dark-5 cursor-pointer hover:scale-110 transition-transform" @click="generalStore.toggleCollapseNavbar" />
         </div>
         <ul class="flex flex-col p-0 items-center gap-[18px]">
-          <NavLink v-for="link in LINKS" :key="link.name" :name="link.name" :route="link.route" :icon="link.icon" :is-collapsed="collapse" v-tooltip.right="collapse?link.name:''" />
+          <NavLink v-for="link in LINKS" :key="link.name" :name="link.name" :route="link.route" :icon="link.icon" :is-collapsed="is_navbar_collapsed" v-tooltip.right="is_navbar_collapsed?link.name:''" />
         </ul>
       </div>
       
       <div class="p-4">
         <Divider class="!h-[2.5px] bg-[#DED8E1] rounded-full"/>
         <ul class="flex flex-col p-0 items-center gap-4">
-          <NavLink name="Settings" route="settings" :icon="SettingsSVG" :is-collapsed="collapse" v-tooltip.right="collapse?'Settings':''" />
-          <NavLink name="Logout" :icon="LogoutSVG" :is-collapsed="collapse" v-tooltip.right="collapse?'Logout':''" />
+          <NavLink name="Settings" route="settings" :icon="SettingsSVG" :is-collapsed="is_navbar_collapsed" v-tooltip.right="is_navbar_collapsed?'Settings':''" />
+          <NavLink name="Logout" :icon="LogoutSVG" :is-collapsed="is_navbar_collapsed" v-tooltip.right="is_navbar_collapsed?'Logout':''" />
         </ul>
       </div>
     </div>
@@ -40,8 +40,8 @@ import BillingSVG from "@/components/svgs/BillingSVG.vue"
 import SettingsSVG from "@/components/svgs/SettingsSVG.vue"
 import LogoutSVG from "@/components/svgs/LogoutSVG.vue"
 
-const collapse = ref(false)
-const toggle_collapse = () => collapse.value = !collapse.value
+const generalStore = useGeneralStore()
+const { is_navbar_collapsed } = storeToRefs(generalStore)
 
 type NavLink = {
   name: string
